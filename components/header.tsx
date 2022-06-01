9f467f71dff41be4df4c27f45ff59dcff49d3f90
radicalDilettante <wonjun92@gmail.com>
2022-06-01 23:43:17 +1200

    add header

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <button>My Cart ( 4 )</button>
+    </div>
+  );
+}
