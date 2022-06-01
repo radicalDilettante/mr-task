1f0069e4815a55730600ca5a4e208c67d6edc031
radicalDilettante <wonjun92@gmail.com>
2022-06-02 02:22:45 +1200

    implement mobile layout

--- components/contents.tsx
+++ components/contents.tsx
@@ -17,44 +17,46 @@ export default function Contents({ data, cart, setCart }: IProps) {
       <div className={styles.image_wrapper}>
         <img src={data.imageURL} />
       </div>
-      <div className={styles.detail}>
-        <h1>{data.title}</h1>
-        <p className={styles.price}>${data.price.toFixed(2)}</p>
-        <p className={styles.desc}>{data.description}</p>
-        <p className={styles.size}>
-          Size<span className={styles.star}>*</span>{" "}
-          <span className={styles.size_status}>{size}</span>
-        </p>
-        <ul>
-          {data.sizeOptions.map((item, index) => (
-            <SizeItem
-              key={index}
-              label={item.label}
-              sizeStatus={size}
-              setSize={setSize}
-            />
-          ))}
-        </ul>
-        <button
-          className={styles.button}
-          onClick={() => {
-            if (size) {
-              const newCart = cart.slice();
+      <div className={styles.detail_wrapper}>
+        <div className={styles.detail}>
+          <h1>{data.title}</h1>
+          <p className={styles.price}>${data.price.toFixed(2)}</p>
+          <p className={styles.desc}>{data.description}</p>
+          <p className={styles.size}>
+            Size<span className={styles.star}>*</span>{" "}
+            <span className={styles.size_status}>{size}</span>
+          </p>
+          <ul>
+            {data.sizeOptions.map((item, index) => (
+              <SizeItem
+                key={index}
+                label={item.label}
+                sizeStatus={size}
+                setSize={setSize}
+              />
+            ))}
+          </ul>
+          <button
+            className={styles.button}
+            onClick={() => {
+              if (size) {
+                const newCart = cart.slice();
 
-              if (cart.find((i) => i.size === size)) {
-                const index = newCart.findIndex((i) => i.size === size);
-                newCart[index].qty++;
-              } else {
-                newCart.push({ data: data, size: size, qty: 1 });
+                if (cart.find((i) => i.size === size)) {
+                  const index = newCart.findIndex((i) => i.size === size);
+                  newCart[index].qty++;
+                } else {
+                  newCart.push({ data: data, size: size, qty: 1 });
+                }
+
+                setCart(newCart);
               }
-
-              setCart(newCart);
-            }
-            setSize(undefined);
-          }}
-        >
-          Add To Cart
-        </button>
+              setSize(undefined);
+            }}
+          >
+            Add To Cart
+          </button>
+        </div>
       </div>
     </section>
   );
